023c2d538180b8c4b5192f2007d4edc112b45280
Ben Math <benjamin.aaron.math@gmail.com>
2022-10-18 18:55:02 -0400

    changed navbar upon login

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -3,42 +3,81 @@ import { NavLink } from "react-router-dom";
 
 import "./Header.css";
 function Header({ user }) {
-  console.log(user)
-  return (
-    <div className="header-container">
-      <div className="header-container-title">
-        <h1>Noble Movie App</h1>
-      </div>
+  const headerMessage = user ? `Welcome, ${user.username}!` : "Movie App"
+  if (user == null) {
+    return (
+      <div className="header-container">
+        <div className="header-container-title">
+          <h1>{headerMessage}</h1>
+        </div>
 
-      <div className="header-link">
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive ? "active-nav-link" : undefined
-          }
-          end
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/sign-up"
-          className={({ isActive }) =>
-            isActive ? "active-nav-link" : undefined
-          }
-        >
-          Sign up
-        </NavLink>
-        <NavLink
-          to="/login"
-          className={({ isActive }) =>
-            isActive ? "active-nav-link" : undefined
-          }
-        >
-          Login
-        </NavLink>
+        <div className="header-link">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "active-nav-link" : undefined
+            }
+            end
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/sign-up"
+            className={({ isActive }) =>
+              isActive ? "active-nav-link" : undefined
+            }
+          >
+            Sign up
+          </NavLink>
+          <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              isActive ? "active-nav-link" : undefined
+            }
+          >
+            Login
+          </NavLink>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    console.log(user)
+    return (
+      <div className="header-container">
+        <div className="header-container-title">
+          <h1>{headerMessage}</h1>
+        </div>
+
+        <div className="header-link">
+          <NavLink
+            to="/movie"
+            className={({ isActive }) =>
+              isActive ? "active-nav-link" : undefined
+            }
+            end
+          >
+            Movie
+          </NavLink>
+          <NavLink
+            to="/profile"
+            className={({ isActive }) =>
+              isActive ? "active-nav-link" : undefined
+            }
+          >
+            Profile
+          </NavLink>
+          <NavLink
+            to="/logout"
+            className={({ isActive }) =>
+              isActive ? "active-nav-link" : undefined
+            }
+          >
+            Logout
+          </NavLink>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Header;
